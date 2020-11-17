bcf6ed716e4f30c9c44659926862b6193033fec4
colin greeley <colin.greeley@wsu.edu>
2020-11-17 11:27:56 -0800

    Updated RCNN

--- R-CNN.py
+++ R-CNN.py
@@ -1,14 +1,12 @@
 
 ## Colin Greeley ##
-## Original code from pyimagesearch ##
+## IOU, image pyramid, and sliding window code from pyimagesearch ##
 ## https://www.pyimagesearch.com/category/deep-learning/ ##
-## "Turning any CNN image classifier into an object detector with Keras, Tensorflow, and OpenCV"  ##
-## "Intersection over Union (IoU) for object detection" ##
 
 from tensorflow.keras.applications import InceptionResNetV2
 from tensorflow.keras.preprocessing.image import img_to_array, ImageDataGenerator
 from tensorflow.keras.applications import imagenet_utils
-from tensorflow.keras import models, layers
+from tensorflow.keras import models, layers, optimizers
 from tensorflow.keras.models import load_model
 import tensorflow.keras.backend as K
 from imutils.object_detection import non_max_suppression
@@ -16,6 +14,7 @@ from generatetiles import containsWhite
 from collections import namedtuple
 import numpy as np
 from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
 import argparse
 import imutils
 import time
@@ -26,7 +25,7 @@ import json
 
 label_map = {'h':0, 'b':1, 'g':2, 'y':3}
 value_map = ['healthy', 'blue', 'green', 'yellow']
-color_map = [(0,0,0), (255,0,0), (0,255,0), (0,255,255)] #BGR
+color_map = [(0,0,0), (0,0,255), (0,255,0), (255,255,0)] #BGR
 
 def get_args():
     
@@ -69,23 +68,20 @@ def get_data(path, stop=10000):
             bounding_boxes.append(bb)
         bb_array.append(np.array(bounding_boxes, dtype='int32'))
         labels_array.append(labels)
-
+    
     return (images, bb_array, labels_array)
 
 
 class RPN:
-
-    def __init__(self, t):
+    
+    def __init__(self):
         self.WIDTH = 1200
-        self.PYR_SCALE = 1.3    # default: 1.5
+        self.PYR_SCALE = 1.25    # default: 1.5
         self.MIN_SIZE = (300, 300)
         self.ROI_SIZE = (32, 32)
         self.WIN_STEP = int(self.ROI_SIZE[0] / 4)
-        if t == 'test':
-            self.model = None
-        else:
-            self.make_model()
-        self.datagen = ImageDataGenerator(horizontal_flip=True, vertical_flip=True, brightness_range=(-0.2,0.2))
+        self.make_model()
+        self.datagen = ImageDataGenerator(rescale=1/255, horizontal_flip=True, vertical_flip=True, brightness_range=(0.8,1.2))
 
     def sliding_window(self, image, step, ws):
         # slide a window across the image
@@ -238,52 +234,84 @@ class RPN:
     def accuracy(self, y_true, y_pred):
         return K.mean(K.equal(K.round(y_true), K.round(y_pred)))
 
+    def custom_loss(self, y_true, y_pred):  # MSE + higher loss for IOUs that approach 1.0 to deal with class imbalance
+        sqe = K.square(y_true - y_pred) * K.square((y_true * 3) + 1)
+        loss = K.mean(sqe, axis=-1)
+        return loss
+
     def conv_node(self, inputs, filters, kernel):
-        conv_a1 = layers.Conv2D(filters, 1, activation='relu')(inputs)
-        conv_a2 = layers.Conv2D(filters, kernel, padding='same')(conv_a1)
-        norm_a = layers.BatchNormalization()(conv_a2)
-        act_a = layers.Activation('relu')(norm_a)
-        pool_a = layers.MaxPooling2D()(act_a)
-        return pool_a
+        x = layers.Conv2D(filters, kernel, padding='same')(inputs)
+        y = layers.Conv2D(filters, 1)(inputs)
+        x = layers.Add()([y, x])
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.MaxPooling2D()(x)
+        return x
 
     def make_model(self):  
         # region proposal network
         input_layer = layers.Input(shape=(*self.ROI_SIZE, 3))
 
-        l1 = self.conv_node(input_layer, 32, 3)
-        l2 =          self.conv_node(l1, 64, 3)
-        l3 =          self.conv_node(l2, 128, 3)
+        l1 = self.conv_node(input_layer, 64, 3)
+        l2 =          self.conv_node(l1, 128, 3)
+        l3 =          self.conv_node(l2, 256, 3)
 
-        flatten = layers.Flatten()(l3)
-        dense_1 = layers.Dense(256, activation='relu')(flatten)
-        dropout_1 = layers.Dropout(0.3)(dense_1)
-        dense_2 = layers.Dense(64, activation='relu')(dropout_1)
-        dropout_2 = layers.Dropout(0.2)(dense_2)
-        output_layer = layers.Dense(1, activation='sigmoid')(dropout_2)
+        x = layers.Flatten()(l3)
+        x = layers.Dense(512)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.Dropout(0.3)(x)
+        x = layers.Dense(64, activation='relu')(x)
+        x = layers.Dropout(0.1)(x)
+        output_layer = layers.Dense(1, activation='sigmoid')(x)
 
         model = models.Model(input_layer, output_layer)
-        model.compile(optimizer='rmsprop', loss='mse', metrics=[self.accuracy])
+        model.compile(optimizer=optimizers.Adam(), loss=[self.custom_loss], metrics=[self.accuracy])
         #model.summary()
         self.model = model
 
+    def load(self, name):
+        #self.model.load_weights(name)
+        self.model = load_model(name, compile=False)
+        self.model.compile(optimizer=optimizers.Adam(), loss=[self.custom_loss], metrics=[self.accuracy])
 
-    def train(self, rois_array, IoUs_array):
+    def cleave_data(self, x, y):
+        new_x = []
+        new_y = []
+        i = 0
+        while i < len(y):
+            if y[i] + 0.5 > np.random.random():
+                new_x.append(x[i]) 
+                new_y.append(y[i]) 
+            i += 1
+        return np.array(new_x), np.array(new_y)
+
+    def train(self, rois_array, IoUs_array, batch_size=256):
 
         x = np.concatenate(rois_array, axis=0)
         y = np.concatenate(IoUs_array, axis=0)
+        print("X:", x, " Y:", y)
+        print(sorted(y)[-10:])
         print("X:", x.shape, " Y:", y.shape)
 
-        # normalize RoI pixel values
-        x /= 255.
-        x, y = shuffle(x, y)
-        self.model.fit(self.datagen.flow(x, y, batch_size=32), steps_per_epoch=len(x)//32, epochs=50)
-        self.model.save("RPN.h5")
+        y /= max(y)
+        print(y)
+        #x, y = self.cleave_data(x, y)
+        #plt.hist(y, bins=20)
+        #plt.show()
+        #print("X:", x.shape, " Y:", y.shape)
+        for i in range(5):
+            x, y = shuffle(x, y)
+            self.model.fit(self.datagen.flow(x, y, batch_size=batch_size), steps_per_epoch=len(x)//batch_size, epochs=100)
+            self.model.save_weights("RPN_weights"+str(i)+".h5")
+            self.model.save("RPN"+str(i)+".h5")
 
     def predict(self, roi_images, rois, labels_array=None, confidence=0.3):
 
         print("\n[INFO] Classifying ROIs...")
         start = time.time()
         preds = self.model.predict(roi_images/255.)
+        print(sorted(preds[:,0])[-10:])
         end = time.time()
         print("[INFO] Classifying ROIs took {:.5f} seconds".format(end - start))
         
@@ -315,7 +343,7 @@ class RPN:
        
         orig = image
         (orig_H, orig_W) = orig.shape[:2]
-        orig = imutils.resize(orig, 1000)
+        orig = imutils.resize(orig, 1500)
         (H, W) = orig.shape[:2]
         (Delta_H, Delta_W) = (orig_H / H, orig_W / W)
         clone = orig.copy()
@@ -324,36 +352,43 @@ class RPN:
         for box in boxes:
             (startX, startY, endX, endY) = box
             cv2.rectangle(clone, (int(startX/Delta_W), int(startY/Delta_H)), (int(endX/Delta_W), int(endY/Delta_H)), (0, 0, 0), 1)
-        cv2.imshow("Before", clone)
-        cv2.waitKey(0)
-        cv2.imwrite('./RPN_Prediction.jpg', clone)
+        #cv2.imshow("Before", clone)
+        #cv2.waitKey(0)
+        plt.imshow(clone)
+        plt.show()
+        #cv2.imwrite('./RPN_Prediction.jpg', clone)
         return clone
 
 
 class Classifier:
 
-    def __init__(self, t):
-        if t == 'test':
-            self.model = None
-        else:
-            self.make_model()
-        self.datagen = ImageDataGenerator(horizontal_flip=True, vertical_flip=True, brightness_range=(-0.2,0.2))
+    def __init__(self):
+        self.input_size = (256, 256)
+        self.make_model()
+        self.datagen = ImageDataGenerator(rescale=1/255, horizontal_flip=True, vertical_flip=True, brightness_range=(0.8,1.2))
     
     def make_model(self):
         conv_base = InceptionResNetV2(weights='imagenet', 
                                 include_top=False, 
-                                input_shape=(299, 299, 3))
+                                input_shape=(*self.input_size, 3))
         for layer in conv_base.layers:
             layer.trainable = False
-        pooling = layers.GlobalAveragePooling2D()(conv_base.output)
-        dropout_1 = layers.Dropout(0.3)(pooling)
-        dense = layers.Dense(256, activation='relu')(dropout_1)
-        dropout_2 = layers.Dropout(0.2)(dense)
-        output = layers.Dense(4, activation='sigmoid')(dropout_2)
+        x = layers.GlobalAveragePooling2D()(conv_base.output)
+        x = layers.Dense(512)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.Dropout(0.3)(x)
+        x = layers.Dense(64, activation='relu')(x)
+        x = layers.Dropout(0.1)(x)
+        output = layers.Dense(4, activation='sigmoid')(x)
 
         model = models.Model(conv_base.input, output)
-        model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
         self.model = model
+
+    def load(self, name):
+        self.model.load_weights(name)
+        #self.model = load_model('models/classifier.h5')
 
     def custom_non_max_suppression(self, boxes, probs=None, overlapThresh=0.3):
         if len(boxes) == 0:
@@ -385,39 +420,69 @@ class Classifier:
                 np.where(overlap > overlapThresh)[0])))
         return pick
 
-    def train(self, images, ba, la):
-
-        boxes_array = ba
-        label_array = la
-        # remove 50% of the heathly cells from training set
-        for i in range(len(boxes_array)):
-            j = 0
-            while j < len(label_array[i]):
-                if label_array[i][j] == 0 and np.random.random() < 0.8:
-                    boxes_array[i] = np.delete(boxes_array[i], j, axis=0)
-                    label_array[i] = np.delete(label_array[i], j)
+    def cleave_data(self, ba, la, threshold):
+        boxes_array = []
+        label_array = []
+        for i in range(len(la)):
+            boxes_row = []
+            labels_row = []
+            for j in range(len(la[i])):
+                if la[i][j] == 0 and np.random.random() < threshold:
+                    pass
                 else:
-                    j += 1
+                    boxes_row.append(ba[i][j])
+                    labels_row.append(la[i][j])
+            boxes_array.append(boxes_row)
+            label_array.append(labels_row)
+        return np.array(boxes_array), np.array(label_array)
+
+    def format_data(self, images, boxes_array, label_array):
 
         # extract images from bounding box coordinates
         print("\n[INFO] Extracting images from bounding box coordinates...")
+        start = time.time()
         x = []
         for image, boxes in zip(images, boxes_array):
-            x.append([cv2.resize(image[y1:y2, x1:x2], (299, 299)) / 255. for (x1, y1, x2, y2) in boxes])
+            x.append([cv2.resize(image[y1:y2, x1:x2], self.input_size) for (x1, y1, x2, y2) in boxes])
         
+        end = time.time()
+        print("[INFO] Extracting images took {:.5f} seconds".format(end - start))
+
         x = np.concatenate(x, axis=0)
         y = np.concatenate(label_array, axis=0)
 
         print("X:", x.shape, " Y:", y.shape)
-        print("[INFO] Training classifier...")
-
+        print(len([1 for i in y if i == 0]))
+        print(len([1 for i in y if i == 1]))
+        print(len([1 for i in y if i == 2]))
+        print(len([1 for i in y if i == 3]))
+        
         x, y = shuffle(x, y)
-        self.model.fit(self.datagen.flow(x, y, batch_size=32), steps_per_epoch=len(x)//32, epochs=500)
-        self.model.save("classifier.h5")
+        return x, y
+
+    def train(self, images, ba, la, batch_size=64):
+
+        for i in range(10):
+            print("\nEpisode:", i+1)
+            boxes_array, label_array = self.cleave_data(ba, la, 0.85)
+            x, y = self.format_data(images, boxes_array, label_array)
+            #for j,k in zip(x[:5],y[:5]):
+            #    plt.figure(figsize=(16,9))
+            #    plt.imshow(x)
+            #    plt.title(str(y))
+            #    plt.show()
+            self.model.fit(self.datagen.flow(x, y, batch_size=batch_size), steps_per_epoch=len(x)//batch_size, epochs=100)
+            x,y,boxes_array,label_array = None, None, None, None
+            del x
+            del y
+            del boxes_array
+            del label_array
+            self.model.save_weights("classifier_weights"+str(i)+".h5")
+            self.model.save("classifier"+str(i)+".h5")
 
     def predict(self, image, probs, boxes, thresh):
         boxes = non_max_suppression(boxes, probs=probs[:,0], overlapThresh=thresh)
-        x = np.array([cv2.resize(image[y1:y2, x1:x2], (299, 299)) / 255. for (x1, y1, x2, y2) in boxes])
+        x = np.array([cv2.resize(image[y1:y2, x1:x2], self.input_size) / 255. for (x1, y1, x2, y2) in boxes])
         print('\nClassifier prediction input shape:', x.shape)
         print("[INFO] Classifying pathologies...")
         start = time.time()
@@ -430,7 +495,7 @@ class Classifier:
         
         orig = image
         (orig_H, orig_W) = orig.shape[:2]
-        orig = imutils.resize(orig, 1000)
+        orig = imutils.resize(orig, 1200)
         (H, W) = orig.shape[:2]
         (Delta_H, Delta_W) = (orig_H / H, orig_W / W)
         clone = orig.copy()
@@ -449,18 +514,20 @@ class Classifier:
             (startX, startY, endX, endY) = box
             cv2.rectangle(clone, (int(startX/Delta_W), int(startY/Delta_H)), (int(endX/Delta_W), int(endY/Delta_H)), color_map[np.argmax(pred)+1], 1)
             cv2.putText(clone, '{:.0f}%'.format(max(pred)*100), (int(startX/Delta_W), int(startY/Delta_H)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,0,0), 1)
-        cv2.imshow("Before", clone)
-        cv2.waitKey(0)
-        cv2.imwrite('./Classifier_Prediction.jpg', clone)
+        plt.imshow(clone)
+        plt.show()
+        #cv2.imshow("asdf", clone)
+        #cv2.waitKey(0)
+        #cv2.imwrite('./Classifier_Prediction.jpg', clone)
   
 
 class RCNN:
 
     def __init__(self, args):
         self.args = args
-        self.rpn = RPN(args['type'])
-        self.classifier = Classifier(args['type'])
-        self.fully_annotated_images = 5
+        self.rpn = RPN()
+        self.classifier = Classifier()
+        self.fully_annotated_images = 6
 
     def __call__(self, data_path, instruction):
         if instruction == 'train_all':
@@ -503,17 +570,20 @@ class RCNN:
     def test(self, data_path):
         image = cv2.imread(data_path)
         # load pre-trained model
-        self.rpn.model = load_model('RPN.h5')
-        self.classifier.model = load_model('classifier.h5')
+        #self.rpn.load('models/RPN3.h5')
+        self.rpn.load('models/RPN3.h5')
+        #self.rpn.model = load_model('models/RPN3.h5')
+        #self.classifier.load('classifier_weights15.h5')
+        self.classifier.model = load_model('classifier0.h5')
         # get rois
         roi_images, rois = self.rpn.get_rois(image)
         # rpn predictions
-        probs, RPN_boxes, _ = self.rpn.predict(roi_images, rois, confidence=0.3)   # confidence of bounding box being over cell
+        probs, RPN_boxes, _ = self.rpn.predict(roi_images, rois, confidence=0.1)        # confidence of bounding box being over cell
         # classifier predictions
         preds, boxes = self.classifier.predict(image, probs, RPN_boxes, thresh=0.5)     # non-max suppression threshold (higher number allows more overlap)
         # show output                                                                    
         self.rpn.show_pred(image, probs, RPN_boxes, thresh=0.5)                         # non-max suppression threshold
-        self.classifier.show_pred(image, preds, boxes, confidence=0.7)                  # confidence of pathology classification
+        self.classifier.show_pred(image, preds, boxes, confidence=0.5)                  # confidence of pathology classification
 
 
 
